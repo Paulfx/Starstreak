504a6c5e83d21846381ce12b84cf1c434aa32a70
p1410782 <devnull@localhost>
2017-03-07 09:43:53 +0100

    G: Enter commit mess.  Lines beginning with 'HG:' are removed.

---
+++ src/terrein.h
@@ -0,0 +1,28 @@
+#ifndef PARTIE_H
+#define PARTIE_H
+
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <assert.h>
+#include <partition.h>
+#include <jeu.h>
+#include <score.h>
+
+/**
+ @brief la classe qui teste le succés d'une note et modifie le score
+ */
+
+class Terrein {
+    
+private:
+    Partition part;
+    line tab[8];
+    float score;
+    
+    
+public:
+    Terrein();
+    
+    Terrein(   );
+};
