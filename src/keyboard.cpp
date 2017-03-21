83fe5a3d7c83b21528430f36369878244ae973df
Theo <devnull@localhost>
2017-03-21 11:58:57 +0100

    keyboard0

---
+++ src/keyboard.cpp
@@ -0,0 +1,36 @@
+//
+//  Keyboard.cpp
+//  
+//
+//  Created by Rabut Théo on 21/03/2017.
+//
+//
+
+#include "Keyboard.hpp"
+
+
+keyboard::keyboard(){
+    keyTab=new bool[6];
+    for(int i=0;i<6;i++){
+        keyTab[i]=false;
+    }
+}
+
+keyboard::~keyboard(){
+    delete [] keyTab;
+}
+
+
+
+void keyboard::setKey(const unsigned int i){
+    if(keyTab[i]){
+        keyTab[i]=false;
+    }else{
+        keyTab[i]=true;
+    }
+}
+
+
+bool* keyboard::getKey(){
+    return keyTab;
+}
