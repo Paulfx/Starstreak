1b562144d2d5941432f897465a2768ecca5c6226
Lafoix <devnull@localhost>
2017-03-21 10:35:19 +0100

    code menu

--- src/game.cpp
+++ src/game.cpp
@@ -1,24 +1,3 @@
 #include "game.h"
 
 using namespace std;
-
-Partie::Partie(){
-    Terrain part();
-    
-}
-
-Partie::Partie(const string & filename,int difficulty){
-    Partition part(const string & filename,int difficulty);
-    score=0;
-    noteStreak=0;
-    startPower=0;
-}
-
-Partie::~Partie(){
-    ~Partition();
-    score=0;
-    noteStreak=0;
-    startPower=0;
-    
-}
-
--- src/menu.cpp
+++ src/menu.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-
 Menu::Menu() {
 
 }
@@ -11,14 +10,41 @@ Menu::Menu(const string& filename) {
 	cout<<"Ouverture de : "<<filename<<endl;
 	ifstream fichier(filename.c_str());
 	assert(fichier.is_open());
-	fichier >> nbLine;
-	fichier >> duree;
+	
+	string line;
+	int nbSongs;
 
-	partition = new line[nbLine];
+	fichier>>nbSongs;
 
-	for(unsigned int i=0;i<nbLine;i++) {
-		fichier >> partition[i].data >> partition[i].time;
+
+	songTab = new Song* [nbSongs];
+
+	while(getline(fichier,line)) {
+
+		stringstream lineStream(line); //conversion d'un str en stream
+		string word;
+		Song song = new Song;
+		int i=0;
+
+		while(getline(lineStream,word,',')) {
+			switch(i) {
+				case 0 : song.title = word; break;
+				case 1 : song.filePartition = word; break;
+				case 2 : song.fileMusic = word; break;
+				case 3 : song.fileImage = word; break;
+				case 4 : song.duration = word; break;
+				default : break;
+			}
+
+			i++;
+			
+		}
+
 	}
 
 	fichier.close();
+}
+
+void Menu::afficher() {
+
 }
