cd877b850d3d8255b22b51527a07edc3a482e151
p1611978 <devnull@localhost>
2017-03-24 13:41:54 +0100

    Plus de problème constructeur menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -12,11 +12,57 @@ Menu::Menu(const string& filename) {
 	cout<<"Ouverture de : "<<filename<<endl;
 	ifstream fichier(filename.c_str());
 	assert(fichier.is_open());
+
+
 	string line;
-	unsigned int i=0,j;
+	unsigned int i,j=0;
 	Song* song;
 
+	string word;
+
+
+	fichier>>nbSongs;
+	songTab = new Song*[nbSongs];
+
+	while(getline(fichier,line)) {
+		
+		if(!line.empty()) {
+			stringstream lineStream(line);
+			i=0;
+			song = new Song;
+
+			
+
+			while(getline(lineStream,word,',')) {
+
+				switch(i) {
+					case 0 : song->title = word; break;
+					case 1 : song->filePartition = word; break;
+					case 2 : song->fileMusic = word; break;
+					case 3 : song->fileImage = word; break;
+					case 4 : song->duration = atof(word.c_str()); break;
+					default : break;
+				}
+
+				i++;
+			}
+
+			//cout<<song->title<<song->fileImage<<endl;
+			
+			songTab[j] = song;
+			j++;
+			song = NULL;
+
+		}
+	}
+
+
+
+
+
+/*
 	fichier>>nbSongs; //Récupération du nombre de chansons (donnée membre)
+
 
 
 	songTab = new Song* [nbSongs];
@@ -30,6 +76,7 @@ Menu::Menu(const string& filename) {
 		song = new Song;
 		j=0;
 		while(getline(lineStream,word,',')) {
+			cout<<word;
 			switch(j) {
 				case 0 : song->title = word; break;
 				case 1 : song->filePartition = word; break;
@@ -49,22 +96,20 @@ Menu::Menu(const string& filename) {
 	afficher();
 
 
-	currPtr = songTab[0];
+	currPtr = songTab[0];*/
 
-	
-	cout<<"gzehueghuzeghghzeguhguzehguz";
 }
 
 
 Menu::~Menu() {
-	cout <<"DESTRU 1111";
-	for(unsigned int i=0;i<nbSongs;++i) {
+
+
+	for(unsigned int i =0;i<nbSongs;i++) {
 		delete songTab[i];
 	}
+	
 	delete songTab;
 	songTab = NULL;
-
-	cout<<"DESTRUUUU 22222";
 }
 
 void Menu::afficher() {
