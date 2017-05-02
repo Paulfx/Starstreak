bbffb78d662fd3238b77e894f78d07486a5ce17d
Lafoix <devnull@localhost>
2017-05-02 09:39:42 +0200

    vittesse 2

--- src/core/game.cpp
+++ src/core/game.cpp
@@ -9,7 +9,7 @@ const int COLUMN_NOTE_3 = 450;
 const int COLUMN_NOTE_4 = 600;
 const int BEGIN_CADRE = 0;
 const int END_VALIDATION = 600;
-const float TIME_UNTIL_DOWN = 3; //dépend de difficulté
+const float TIME_UNTIL_DOWN = 2; //dépend de difficulté
 
 Game::Game() {
 
